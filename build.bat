433ffb89f552cf0792eaad1d2e29f283daf6f05c
masukomi <masukomi@masukomi.org>
2019-06-21 22:55:53 -0400

    build scripts now rebuild etc and format in case changed

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
+"C:\Program Files (x86)\Chicken Scheme\bin\csc" -s etc.scm -j etc
+"C:\Program Files (x86)\Chicken Scheme\bin\csc" -s format.scm -j format
 "C:\Program Files (x86)\Chicken Scheme\bin\csc" -static main.scm -o scheme-format
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+csc -s etc.scm -j etc
+csc -s format.scm -j format
 csc -static main.scm -o scheme-format
